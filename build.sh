3edc8b885155690982aaaf98b6efb0faddd1c1ab
Your Name <you@example.com>
2025-06-23 13:05:20 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-#source /etc/profile
+
+export pwd="$(pwd)"
 export INCLUDE_FLAG="-I${pwd}/include/linux"
 export DRV_INCLUDE_FLAG=-I${pwd}/arch/arm/mach-balong/include 
 export ASM_INCLUDE_FLAG="-I${pwd}/arch/arm/include/asm"
-export PATH=/opt/4.5.1/bin/:$PATH
 
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/opt/4.5.1/bin/arm-linux-
+export CROSS_COMPILE=${pwd}/android_toolchains/arm-2010q1/bin/arm-none-linux-gnueabi-
 
-FEATURE_WIFI=FEATURE_ON
-export FEATURE_WIFI
+if ! [ -e android_toolchains ]; then git clone https://github.com/milaq/android_toolchains.git ; fi
+
+export PATH=$CROSS_COMPILE:$PATH
+export FEATURE_WIFI=FEATURE_ON
+
 
 make hisi_balong_hi6920cs_e5776s_defconfig
-make  -C ./ -j 64 BOARD_TYPE=BOARD_ASIC VERSION_TYPE=CHIP_BB_6920CS PRODUCT_CFG_BUILD_TYPE= 2>log.txt
+make  -C ./ -j $(nproc) BOARD_TYPE=BOARD_ASIC VERSION_TYPE=CHIP_BB_6920CS PRODUCT_CFG_BUILD_TYPE= 2>log.txt
+
 if [ 0 -ne $? ]
 then
 	echo "############################# "
 	echo "Make KERNEL Failed!"
+	echo "Check log.txt for details"
 	echo "#############################"
 	exit 1
 else
